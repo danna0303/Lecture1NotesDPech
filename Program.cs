fb9a9b73467fa594df243d9bce2886c469fcebf9
dpech <dpech@DANNA>
2024-06-11 23:40:34 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Lecture1NotesDPech
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            Console.WriteLine("Hello my Name is Danna");
+
+
+            // To comment Code,do double forward slashs //
+            //Console gives us access to working with the console
+            //CODE IS CASE SENSTIVE
+            //Dot,., is used to access the different ways to interact with your code
+            //Parentheses are used to RUN our code and pass in differnet arguments
+            //semi colon, the end of a single statement
+            // Curly braces define code blocks
+            // Compile Time Error
+            //Is an Error that visual studio will warn you about, that there is improper syntax someplace. Needs to be fixed before you can build your code.
+            //Errors in the Error list will "Help" you find the problem
+            // Double quotes tell the computer that everything in between is a string
+            //To get project setup with Github
+            //1. Open View-> GitChanges
+        }
+    }
+}
